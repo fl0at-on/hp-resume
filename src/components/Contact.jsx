adf9fcd7d256071ca9d7934c30ca04ea58152fed
fl0at-on <megaw.mark@gmail.com>
2019-02-12 18:36:46 -0600

    Add alert to Contact textarea

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,11 +11,16 @@ const Contact = () => {
     fontSize: "18px"
   };
 
+  const submitForm = e => {
+    e.preventDefault();
+    alert("Mischief Managed. FYI - what you typed didn't go anywhere.");
+  };
+
   return (
     <div>
       <h1>Send an Owl?</h1>
-      <form>
-        <textarea />
+      <form onSubmit={e => submitForm(e)}>
+        <textarea placeholder="I solemnly swear I am up to no good." />
         <button>Submit</button>
       </form>
     </div>
